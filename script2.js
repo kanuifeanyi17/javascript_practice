d57afa1d6ac4b4c3cdaa73bf3f5204333d8966c7
kanuifeanyi17 <56242217+kanuifeanyi17@users.noreply.github.com>
2021-03-05 09:20:23 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+//a program that asks the user for their name and greets them with their name
+
+function greetUser() {
+const myName = prompt("please enter your name", "");
+alert ("Welcome " + myName + "!");
+} ;
+
+greetUser();
