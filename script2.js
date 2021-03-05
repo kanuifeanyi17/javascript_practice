d9668be232a0f9cb0b38543e11f25df9387c1f2f
kanuifeanyi17 <56242217+kanuifeanyi17@users.noreply.github.com>
2021-03-05 09:31:19 +0100

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-//a program that asks the user for their name and greets them with their name
-
-function greetUser() {
-const myName = "Alice & Bob";
-alert ("Welcome " + myName + "!");
-} ;
-
-greetUser();
